5439bb7e6412783d55ca729d729cd7f609943a4f
Abdelrahman Badran <badrannn90@gmail.com>
2024-10-05 14:13:14 +0300

    add update transaction

--- app/sidekiq/count_updater_job.rb
+++ app/sidekiq/count_updater_job.rb
@@ -3,12 +3,16 @@ class CountUpdaterJob
   queue_as :count_updater
 
   def perform()
-    Application.find_each do |application|
-      application.update(chats_count: application.chats.count)
-    end
+    begin
+      ActiveRecord::Base.transaction do
+        Application.find_each do |application|
+          application.update!(chats_count: application.chats.count)
+        end
 
-    Chat.find_each do |chat|
-      chat.update(messages_count: chat.messages.count)
-    end
+        Chat.find_each do |chat|
+          chat.update(messages_count: chat.messages.count)
+        end
+      end
+    end 
   end
 end
